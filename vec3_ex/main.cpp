7bc2f027d03c78150020a5a2577947a4c24617eb
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2018-10-07 23:18:19 +0200

    main.cpp tests updated

--- vec3_ex/main.cpp
+++ vec3_ex/main.cpp
@@ -5,7 +5,13 @@ int main()
 {
 	Vec3<int> a;
 	Vec3<int> b(2,2,1); 
-	Vec3<double> c(1,1,4);
-	c.Normalize();
-	printf("distance :%.3f", c.x);
+	Vec3<int> c(3, 1, 3);
+	printf("distance :%.3f\n", c.distance_to(b));
+
+	Vec3<int> d = b + c;
+	printf("d.x:%d\n", d.x);
+
+	Vec3<double> f(1,1,4);
+	f.Normalize();
+	printf("normalized f.x :%.3f\n", f.x);
 }
